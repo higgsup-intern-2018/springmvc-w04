6e571400f1eb159dd163922d0ea7839f7cbceeeb
Jon <hungnh.uet@gmail.com>
2018-07-22 22:13:07 +0700

    Global Exception Handling config

---
+++ src/main/java/com/higgsup/intern/ebshop/exception/ResourceNotFoundException.java
@@ -0,0 +1,19 @@
+package com.higgsup.intern.ebshop.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+public class ResourceNotFoundException extends ServiceException {
+    public ResourceNotFoundException() {
+        super();
+    }
+    public ResourceNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+    public ResourceNotFoundException(Throwable cause) {
+        super(cause);
+    }
+}
