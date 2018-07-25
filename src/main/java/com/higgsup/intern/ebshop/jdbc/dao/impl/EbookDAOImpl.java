9db6300867d5475cd10710d6cdd9e73de4877370
Thieu Tu <tu.thieuthanh@gmail.com>
2018-07-25 11:43:40 +0700

    add EbookDAOImpl

--- src/main/java/com/higgsup/intern/ebshop/jdbc/dao/impl/EbookDAOImpl.java
+++ src/main/java/com/higgsup/intern/ebshop/jdbc/dao/impl/EbookDAOImpl.java
@@ -1,7 +1,55 @@
 package com.higgsup.intern.ebshop.jdbc.dao.impl;
 
+import com.higgsup.intern.ebshop.jdbc.dao.EbookDAO;
+import com.higgsup.intern.ebshop.jdbc.mapper.EbookMapper;
+import com.higgsup.intern.ebshop.jdbc.model.Ebook;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public class EbookDAOImpl {
+public class EbookDAOImpl implements EbookDAO {
+    private final JdbcTemplate jdbcTemplate;
+    private final NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+    private final EbookMapper ebookMapper;
+
+    public EbookDAOImpl(JdbcTemplate jdbcTemplate,
+                         NamedParameterJdbcTemplate namedParameterJdbcTemplate,
+                         EbookMapper ebookMapper) {
+        this.jdbcTemplate = jdbcTemplate;
+        this.namedParameterJdbcTemplate = namedParameterJdbcTemplate;
+        this.ebookMapper = ebookMapper;
+    }
+    @Override
+    public List<Ebook> find(String name, Long authorId, Long publisherId, Long priceFrom, Long priceTo, String isbn) {
+        return null;
+    }
+
+    @Override
+    public List<Ebook> findTop10BestSellerEbooks() {
+        return null;
+    }
+
+    @Override
+    public void create(Ebook ebook) {
+        SqlParameterSource paramSource = new BeanPropertySqlParameterSource(ebook);
+        String sql = "INSERT INTO ebook(isbn, title, description, author_id, publisher_id, publication_date, pages, price, quantity, deleted) " +
+                "VALUES (:isbn, :title, :description, :authorId, :publisherId, :publicationDate, :pages, :price, :quantity, :deleted);";
+        namedParameterJdbcTemplate.update(sql, paramSource);
+    }
+
+    @Override
+    public void update(Ebook ebook) {
+
+    }
+
+    @Override
+    public void delete(Long id) {
+
+    }
 }
