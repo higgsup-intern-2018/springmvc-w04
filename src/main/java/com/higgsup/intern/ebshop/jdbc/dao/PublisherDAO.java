cc2dd88f5c15dec6fb5b25b075131724fb53b481
tuanhiep <nguyennhutuanhiep@gmail.com>
2018-07-25 17:18:16 +0700

    Delete Publisher

--- src/main/java/com/higgsup/intern/ebshop/jdbc/dao/PublisherDAO.java
+++ src/main/java/com/higgsup/intern/ebshop/jdbc/dao/PublisherDAO.java
@@ -2,4 +2,5 @@ package com.higgsup.intern.ebshop.jdbc.dao;
 
 public interface PublisherDAO {
     void delete(Long id);
+    Integer countBook(Long id);
 }
--- src/main/java/com/higgsup/intern/ebshop/service/impl/PublisherService.java
+++ src/main/java/com/higgsup/intern/ebshop/service/impl/PublisherService.java
@@ -3,7 +3,6 @@ package com.higgsup.intern.ebshop.service.impl;
 import com.higgsup.intern.ebshop.dto.PublisherDTO;
 import com.higgsup.intern.ebshop.exception.ServiceException;
 import com.higgsup.intern.ebshop.jdbc.dao.PublisherDAO;
-import com.higgsup.intern.ebshop.jdbc.model.Publisher;
 import com.higgsup.intern.ebshop.service.IPublisherService;
 import ma.glasnost.orika.MapperFacade;
 import org.springframework.stereotype.Service;
@@ -21,6 +20,11 @@ public class PublisherService implements IPublisherService {
 
     @Override
     public void delete(Long id) {
+
+        if(publisherDAO.countBook(id) > 0)
+        {
+            throw new ServiceException(String.format("Publisher with id = %d still have book in it!", id));
+        }
         publisherDAO.delete(id);
     }
 }
