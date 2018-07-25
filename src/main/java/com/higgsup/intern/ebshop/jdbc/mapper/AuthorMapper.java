a749f1cd62b8b67d53451a92a178a71a9c20073e
Thieu Tu <tu.thieuthanh@gmail.com>
2018-07-25 11:45:27 +0700

    add AuthorMapper

--- src/main/java/com/higgsup/intern/ebshop/jdbc/mapper/AuthorMapper.java
+++ src/main/java/com/higgsup/intern/ebshop/jdbc/mapper/AuthorMapper.java
@@ -1,7 +1,24 @@
 package com.higgsup.intern.ebshop.jdbc.mapper;
 
+import com.higgsup.intern.ebshop.jdbc.model.Author;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Component;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 @Component
-public class AuthorMapper {
+public class AuthorMapper implements RowMapper<Author> {
+    @Override
+    public Author mapRow(ResultSet rs, int i) throws SQLException {
+        Author author = new Author();
+        author.setId(rs.getLong("id"));
+        author.setFirstName(rs.getString("firstname"));
+        author.setLastName(rs.getString("lastname"));
+        author.setYearOfBirth(rs.getInt("yearOfBirth"));
+        author.setDescription(rs.getString("description"));
+        author.setWebsite(rs.getString("website"));
+        author.setOrganization(rs.getString("organization"));
+        return author;
+    }
 }
