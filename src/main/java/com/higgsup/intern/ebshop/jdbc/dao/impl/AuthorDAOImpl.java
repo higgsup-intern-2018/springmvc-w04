8170eb25b73f1a0aa34dde90262283837647644d
anhpth <anhpth.123@gmail.com>
2018-07-25 15:58:58 +0700

    Hai Anh - update author. Done!

--- src/main/java/com/higgsup/intern/ebshop/jdbc/dao/impl/AuthorDAOImpl.java
+++ src/main/java/com/higgsup/intern/ebshop/jdbc/dao/impl/AuthorDAOImpl.java
@@ -1,7 +1,69 @@
 package com.higgsup.intern.ebshop.jdbc.dao.impl;
 
+import com.higgsup.intern.ebshop.jdbc.dao.AuthorDAO;
+import com.higgsup.intern.ebshop.jdbc.mapper.AuthorMapper;
+import com.higgsup.intern.ebshop.jdbc.model.Author;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public class AuthorDAOImpl {
+public class AuthorDAOImpl implements AuthorDAO {
+
+    private final JdbcTemplate jdbcTemplate;
+    private final NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+    private final AuthorMapper authorMapper;
+
+    public AuthorDAOImpl(JdbcTemplate jdbcTemplate, NamedParameterJdbcTemplate namedParameterJdbcTemplate, AuthorMapper authorMapper) {
+        this.jdbcTemplate = jdbcTemplate;
+        this.namedParameterJdbcTemplate = namedParameterJdbcTemplate;
+        this.authorMapper = authorMapper;
+    }
+    @Override
+    public Author findbyId(Long id) {
+        try {
+            SqlParameterSource paramSource = new MapSqlParameterSource("id", id);
+            String sql = "select * from author where id = :id";
+            return namedParameterJdbcTemplate.queryForObject(sql, paramSource, authorMapper);
+        } catch (EmptyResultDataAccessException ex) {
+            return null;
+        }
+    }
+
+    @Override
+    public void update(Author author) {
+        SqlParameterSource paramSource = new BeanPropertySqlParameterSource(author);
+        String sql = "UPDATE author " +
+                "SET firstname = :firstName, " +
+                    "lastname = :lastName, " +
+                    "year_of_birth = :yearOfBirth, " +
+                    "description = :description, " +
+                    "website = :website, " +
+                    "organization = :organization " +
+                "WHERE id = :id";
+        namedParameterJdbcTemplate.update(sql, paramSource);
+    }
+
+    @Override
+    public List<Author> findTop5BestSellingAuthors() {
+        return null;
+    }
+
+    @Override
+    public void create(Author author) {
+
+    }
+
+
+
+    @Override
+    public void delete(Long id) {
+
+    }
 }
