0856faf04aa9a6490a996a467783fb0d675db325
Thieu Tu <tu.thieuthanh@gmail.com>
2018-07-25 11:44:15 +0700

    edit AuthorDAO

--- src/main/java/com/higgsup/intern/ebshop/jdbc/dao/AuthorDAO.java
+++ src/main/java/com/higgsup/intern/ebshop/jdbc/dao/AuthorDAO.java
@@ -5,7 +5,7 @@ import com.higgsup.intern.ebshop.jdbc.model.Author;
 import java.util.List;
 
 public interface AuthorDAO {
-    Author findbyId(Long id);
+    Author findById(Long id);
     List<Author> findTop5BestSellingAuthors();
     void create(Author author);
     void update(Author author);
