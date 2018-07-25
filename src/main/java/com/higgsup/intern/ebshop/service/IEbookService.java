318a81fc309211b1874de1e78c7c3a295aa3dba0
Thieu Tu <tu.thieuthanh@gmail.com>
2018-07-25 11:46:57 +0700

    add IEbookService

--- src/main/java/com/higgsup/intern/ebshop/service/IEbookService.java
+++ src/main/java/com/higgsup/intern/ebshop/service/IEbookService.java
@@ -1,4 +1,10 @@
 package com.higgsup.intern.ebshop.service;
 
+import com.higgsup.intern.ebshop.dto.EbookDTO;
+
 public interface IEbookService {
+    EbookDTO findById(Long id);
+    void create(EbookDTO ebookDTO);
+    void update(EbookDTO ebookDTO);
+    void delete(Long id);
 }
