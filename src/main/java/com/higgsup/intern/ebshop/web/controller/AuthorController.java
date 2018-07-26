23d48f7033ef9d852d0879ac798efb62cb17137c
DucThanh6111996 <Hoahongden123>
2018-07-26 17:19:49 +0700

    AuthorController

--- src/main/java/com/higgsup/intern/ebshop/web/controller/AuthorController.java
+++ src/main/java/com/higgsup/intern/ebshop/web/controller/AuthorController.java
@@ -1,9 +1,32 @@
 package com.higgsup.intern.ebshop.web.controller;
 
+import com.higgsup.intern.ebshop.dto.AuthorDTO;
+import com.higgsup.intern.ebshop.dto.GenericResponseDTO;
+import com.higgsup.intern.ebshop.service.IAuthorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/authors")
 public class AuthorController {
+    private final IAuthorService authorService;
+
+    @Autowired
+    public AuthorController(IAuthorService authorService){
+        this.authorService = authorService;
+    }
+
+    @PostMapping
+    public ResponseEntity<GenericResponseDTO> createAuthor(@RequestBody AuthorDTO authorDTO) {
+        authorService.create(authorDTO);
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(GenericResponseDTO.created());
+    }
+
 }
