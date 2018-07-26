84a6594dd1f382bfad606ae1371c50d7b48fd987
DucThanh6111996 <Hoahongden123>
2018-07-26 17:14:23 +0700

    EbookController

--- src/main/java/com/higgsup/intern/ebshop/web/controller/EbookController.java
+++ src/main/java/com/higgsup/intern/ebshop/web/controller/EbookController.java
@@ -1,9 +1,31 @@
 package com.higgsup.intern.ebshop.web.controller;
 
+import com.higgsup.intern.ebshop.dto.GenericResponseDTO;
+import com.higgsup.intern.ebshop.service.IEbookService;
+import com.higgsup.intern.ebshop.service.IPersonService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/ebooks")
 public class EbookController {
+    private final IEbookService ebookService;
+
+    @Autowired
+    public EbookController(IEbookService ebookService) {
+        this.ebookService = ebookService;
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<GenericResponseDTO> deleteEbookById(@PathVariable("id") Long id) {
+        ebookService.delete(id);
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(GenericResponseDTO.deleted());
+    }
 }
