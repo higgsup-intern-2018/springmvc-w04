26abcf2c4ab6dc2bfb0687931465c61fa3a3e5b3
Thieu Tu <tu.thieuthanh@gmail.com>
2018-07-26 17:06:59 +0700

    Edit

--- src/main/java/com/higgsup/intern/ebshop/dto/CustomerListDTO.java
+++ src/main/java/com/higgsup/intern/ebshop/dto/CustomerListDTO.java
@@ -1,4 +1,25 @@
 package com.higgsup.intern.ebshop.dto;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CustomerListDTO {
+    private List<CustomerDTO> customerDTOS = new ArrayList<>();
+    private Long totalPrice;
+
+    public List<CustomerDTO> getCustomerDTOS() {
+        return customerDTOS;
+    }
+
+    public void setCustomerDTOS(List<CustomerDTO> customerDTOS) {
+        this.customerDTOS = customerDTOS;
+    }
+
+    public Long getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setTotalPrice(Long totalPrice) {
+        this.totalPrice = totalPrice;
+    }
 }
