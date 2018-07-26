485bdcdff445b03447231d9c6194ba692f33b8b9
tuanhiep <nguyennhutuanhiep@gmail.com>
2018-07-26 15:57:57 +0700

    Order Export

--- src/main/java/com/higgsup/intern/ebshop/jdbc/mapper/PublisherMapper.java
+++ src/main/java/com/higgsup/intern/ebshop/jdbc/mapper/PublisherMapper.java
@@ -1,24 +1,7 @@
 package com.higgsup.intern.ebshop.jdbc.mapper;
 
-import com.higgsup.intern.ebshop.jdbc.model.Publisher;
-import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Component;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
-
 @Component
-public class PublisherMapper implements RowMapper<Publisher> {
-
-    @Override
-    public Publisher mapRow(ResultSet rs, int rowNum) throws SQLException {
-        Publisher publisher = new Publisher();
-        publisher.setId(rs.getLong("id"));
-        publisher.setName(rs.getString("name"));
-        publisher.setWebsite(rs.getString("website"));
-        publisher.setFounder(rs.getString("founder"));
-        publisher.setFoundedYear(rs.getInt("founded_year"));
-        publisher.setAddress(rs.getString("address"));
-        return publisher;
-    }
+public class PublisherMapper{
 }
--- src/main/java/com/higgsup/intern/ebshop/service/IPublisherService.java
+++ src/main/java/com/higgsup/intern/ebshop/service/IPublisherService.java
@@ -1,9 +1,4 @@
 package com.higgsup.intern.ebshop.service;
 
-import com.higgsup.intern.ebshop.dto.PublisherDTO;
-
-
 public interface IPublisherService {
-    void create(PublisherDTO publisherDTO);
-    void delete(Long id);
 }
