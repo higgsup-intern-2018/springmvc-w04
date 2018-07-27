22416b31598cfd30d4df5f9032d41b0c4e8cb3d7
Thieu Tu <tu.thieuthanh@gmail.com>
2018-07-27 15:34:28 +0700

    Delete

--- src/main/java/com/higgsup/intern/ebshop/jdbc/mapper/CustomerMapper.java
+++ src/main/java/com/higgsup/intern/ebshop/jdbc/mapper/CustomerMapper.java
@@ -1,23 +1,8 @@
 package com.higgsup.intern.ebshop.jdbc.mapper;
 
-import com.higgsup.intern.ebshop.jdbc.model.Customer;
-import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Component;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
-
 @Component
-public class CustomerMapper implements RowMapper<Customer> {
-    @Override
-    public Customer mapRow(ResultSet rs, int i) throws SQLException {
-        Customer customer = new Customer();
-        customer.setId(rs.getLong("id"));
-        customer.setFirstName(rs.getString("firstname"));
-        customer.setLastName(rs.getString("lastname"));
-        customer.setEmail(rs.getString("email"));
-        customer.setPhone(rs.getString("phone"));
-        customer.setAddress(rs.getString("address"));
-        return customer;
-    }
+public class CustomerMapper {
+
 }
--- src/main/java/com/higgsup/intern/ebshop/web/controller/CustomerController.java
+++ src/main/java/com/higgsup/intern/ebshop/web/controller/CustomerController.java
@@ -1,27 +1,10 @@
 package com.higgsup.intern.ebshop.web.controller;
 
-import com.higgsup.intern.ebshop.dto.CustomerListDTO;
-import com.higgsup.intern.ebshop.service.ICustomerService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/statistics")
 public class CustomerController {
-    private final ICustomerService customerService;
-
-    @Autowired
-    public CustomerController(ICustomerService customerService) {
-        this.customerService = customerService;
-    }
-
-    @GetMapping("/top-5-best-buy-customers")
-    public ResponseEntity<CustomerListDTO> getTop5BestBuyCustomers() {
-        CustomerListDTO customerListDTO = customerService.findTop5BestBuyCustomers();
-        return ResponseEntity.ok(customerListDTO);
-    }
 
 }
