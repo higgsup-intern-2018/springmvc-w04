bbb928510a4fcced74059b412b5445101260a6d6
Thieu Tu <tu.thieuthanh@gmail.com>
2018-07-27 14:11:49 +0700

    Add ICustomerService

--- src/main/java/com/higgsup/intern/ebshop/service/ICustomerService.java
+++ src/main/java/com/higgsup/intern/ebshop/service/ICustomerService.java
@@ -1,4 +1,8 @@
 package com.higgsup.intern.ebshop.service;
 
+import com.higgsup.intern.ebshop.dto.CustomerListDTO;
+
+
 public interface ICustomerService {
+    CustomerListDTO findTop5BestBuyCustomers();
 }
