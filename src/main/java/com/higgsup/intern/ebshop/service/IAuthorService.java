f984b39e332a48b64fdf28470ed5ac0002334c68
DucThanh6111996 <Hoahongden123>
2018-07-27 17:51:13 +0700

    AuthorController

--- src/main/java/com/higgsup/intern/ebshop/service/IAuthorService.java
+++ src/main/java/com/higgsup/intern/ebshop/service/IAuthorService.java
@@ -1,7 +1,9 @@
 package com.higgsup.intern.ebshop.service;
 
 import com.higgsup.intern.ebshop.dto.AuthorDTO;
+import com.higgsup.intern.ebshop.dto.AuthorListDTO;
 
 public interface IAuthorService {
     void create(AuthorDTO authorDTO);
+    AuthorListDTO findTop5BestSellingAuthors();
 }
