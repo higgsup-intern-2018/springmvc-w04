54e7b8f30b0e62f81c53f6f4d4e81e8f9a6dcd84
DucThanh6111996 <Hoahongden123>
2018-07-28 11:39:10 +0700

    EbookService

--- src/main/java/com/higgsup/intern/ebshop/service/impl/EbookService.java
+++ src/main/java/com/higgsup/intern/ebshop/service/impl/EbookService.java
@@ -1,7 +1,27 @@
 package com.higgsup.intern.ebshop.service.impl;
 
+import com.higgsup.intern.ebshop.exception.ServiceException;
+import com.higgsup.intern.ebshop.jdbc.dao.EbookDAO;
+import com.higgsup.intern.ebshop.service.IEbookService;
+import ma.glasnost.orika.MapperFacade;
 import org.springframework.stereotype.Service;
 
 @Service
-public class EbookService {
+public class EbookService implements IEbookService {
+
+    private final EbookDAO ebookDAO;
+    private final MapperFacade mapper;
+
+    public EbookService(EbookDAO ebookDAO, MapperFacade mapper) {
+        this.ebookDAO = ebookDAO;
+        this.mapper = mapper;
+    }
+
+    @Override
+    public void delete(Long id) {
+        if (ebookDAO.findById(id) == null) {
+            throw new ServiceException(String.format("Ebook with id = %d does not exist!", id));
+        }
+        ebookDAO.delete(id);
+    }
 }
