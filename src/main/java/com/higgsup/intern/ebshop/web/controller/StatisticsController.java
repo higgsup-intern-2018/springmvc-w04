4a536dc9e9d83fcd77f548053c48ffa59514a58e
DucThanh6111996 <Hoahongden123>
2018-07-28 10:55:58 +0700

    StatisticController

--- src/main/java/com/higgsup/intern/ebshop/web/controller/StatisticsController.java
+++ src/main/java/com/higgsup/intern/ebshop/web/controller/StatisticsController.java
@@ -1,9 +1,26 @@
 package com.higgsup.intern.ebshop.web.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+        import com.higgsup.intern.ebshop.dto.AuthorListDTO;
+        import com.higgsup.intern.ebshop.service.IAuthorService;
+        import org.springframework.beans.factory.annotation.Autowired;
+        import org.springframework.http.ResponseEntity;
+        import org.springframework.web.bind.annotation.GetMapping;
+        import org.springframework.web.bind.annotation.RequestMapping;
+        import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/statistics")
 public class StatisticsController {
+    private final IAuthorService authorService;
+
+    @Autowired
+    public StatisticsController(IAuthorService authorService) {
+        this.authorService = authorService;
+    }
+
+    @GetMapping("/top-5-best-selling-authors")
+    public ResponseEntity<AuthorListDTO> getTop5Author() {
+        AuthorListDTO top5Author = authorService.findTop5BestSellingAuthors();
+        return ResponseEntity.ok(top5Author);
+    }
 }
